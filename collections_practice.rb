34e822f72de5da4fe90c3a0cd1dce2a35f9096fa
wei chen <weichen9083@gmail.com>
2019-09-06 00:58:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 
+def sort_array_asc(integer)
+  integer.sort 
+end 
+
+def sort_array_desc(integer)
+  integer.sort {|x,y| y<=>x}
+end 
+
+def sort_array_char_count(integer)
+   integer.sort{|x,y| x.length <=>y.length}
+end 
+
+def swap_elements(integer)
+  integer[1],integer[2] = integer[2], integer[1]
+  integer 
+end 
